ae9c5de213655137c5486e4d4ef3f33047c4f66e
VladCO <vlad.condurachi-olivotto@ionos.com>
2024-08-17 14:30:15 +0300

    Modularize exercise verification

--- conditions.py
+++ conditions.py
@@ -1,18 +1,26 @@
 import numpy as np
 
 
+def verify_rep(percentage, direction):
+    if percentage == 100:
+        if direction == 0:
+            print("Half there")
+            return False, 1
+    if percentage == 0:
+        if direction == 1:
+            print("Rep completed")
+            return True, 0
+
+    return False, direction
+
+
 def left_side_lunge_condition(body, direction):
-    percentage = np.interp(body.left_knee_angle, (170, 90), (0, 100))
-    return 85 < body.left_knee_angle < 95 and 170 < body.right_knee_angle < 180
+    left_knee_percentage = np.interp(body.right_knee_angle, (100, 170), (100, 0)) * 0.80
+    right_knee_percentage = np.interp(body.left_knee_angle, (174, 177), (100, 0)) * 0.20
+    percentage = int(left_knee_percentage + right_knee_percentage)
+    return verify_rep(percentage, direction)
 
 
 def left_elbow_bend_condition(body, direction):
     percentage = np.interp(body.right_elbow_angle, (90, 175), (0, 100))
-    if percentage == 100:
-        if direction == 0:
-            return False, 1
-    if percentage == 0:
-        if direction == 1:
-            return True, 0
-
-    return False, direction
+    return verify_rep(percentage, direction)
